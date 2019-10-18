cca46db02faa68333fd1412b11c8f78a0cc9e9ec
Ted Barnett <mail@tedbarnett.com>
2019-10-17 17:02:00 -0700

    Enabled switching to landscape mode

---
+++ Assets/chooseCanvas.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chooseCanvas : MonoBehaviour
+{
+    public GameObject portraitCanvas;
+    public GameObject landscapeCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.deviceOrientation == DeviceOrientation.Portrait || Input.deviceOrientation == DeviceOrientation.PortraitUpsideDown)
+        {
+            //codes for portrait
+            portraitCanvas.SetActive(true);
+            landscapeCanvas.SetActive(false);
+
+        }
+        else
+        {
+            //codes for Landspace;
+            portraitCanvas.SetActive(false);
+            landscapeCanvas.SetActive(true);
+
+        }
+    }
+}
